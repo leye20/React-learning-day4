d6bcd8d79383295c136a201b38055c509a789b68
leye20 <leyeagbo@gmail.com>
2020-04-14 12:00:00 -0600

    add component lifecycle

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
-export default class Navigation extends React.Component {
+interface INavigationProps {
+}
+
+interface INavigationState {
+    updated: boolean
+}
+
+export default class Navigation extends React.Component<INavigationProps, INavigationState> {
     constructor ( props: string )
     {
         super( props );
+        this.state = {updated:true}
         console.log( 'Constructor ran.' );
     }
     componentDidMount ()
@@ -18,6 +26,9 @@ export default class Navigation extends React.Component {
     {
         console.log( '"componentWillUnmount()" ran.' );
     }
+    testUpdate = () => {
+        this.setState( {updated: !this.state.updated} );
+    }
     render ()
     {
         console.log( 'Render ran.' );
@@ -26,7 +37,7 @@ export default class Navigation extends React.Component {
                 <h2>Website Navigation</h2>
                 <ul>
                     <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
+                    <li><a href="#" onClick={this.testUpdate}>About</a></li>
                     <li><a href="#">Services</a></li>
                     <li><a href="#">Contact</a></li>
                 </ul>
