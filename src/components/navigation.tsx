3c8341358b4dfa21a242705185c246bdd6b07230
leye20 <leyeagbo@gmail.com>
2020-04-14 11:27:01 -0600

    componentDidMount added

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -4,9 +4,23 @@ export default class Navigation extends React.Component {
     constructor ( props: string )
     {
         super( props );
+        console.log( 'Constructor ran.' );
+    }
+    componentDidMount ()
+    {
+        console.log( '"componentDidMount()" ran.' );
+    }
+    componentDidUpdate ()
+    {
+        console.log( '"componentDidUpdate()" ran.' );
+    }
+    componentWillUnmount ()
+    {
+        console.log( '"componentWillUnmount()" ran.' );
     }
     render ()
     {
+        console.log( 'Render ran.' );
         return (
             <nav>
                 <h2>Website Navigation</h2>
